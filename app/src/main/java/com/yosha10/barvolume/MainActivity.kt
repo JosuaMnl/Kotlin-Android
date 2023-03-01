5eeb68c4c5366161897d3df95ff7e458e2b46f19
JosuaMnl <josuamnalu@gmail.com>
2023-03-01 17:19:16 +0700

    Finish Project

--- app/src/main/java/com/yosha10/barvolume/MainActivity.kt
+++ app/src/main/java/com/yosha10/barvolume/MainActivity.kt
@@ -6,46 +6,67 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var evWidth: EditText
-    private lateinit var evHeight: EditText
-    private lateinit var evLength: EditText
+    private lateinit var etWidth: EditText
+    private lateinit var etHeight: EditText
+    private lateinit var etLength: EditText
     private lateinit var btnCalculate: Button
     private lateinit var tvResult: TextView
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        evWidth = findViewById(R.id.et_lebar)
-        evHeight = findViewById(R.id.et_tinggi)
-        evLength = findViewById(R.id.et_panjang)
-        btnCalculate = findViewById(R.id.btn_hitung)
-        tvResult = findViewById(R.id.tv_hasil)
+        etWidth = findViewById(R.id.et_width)
+        etHeight = findViewById(R.id.et_height)
+        etLength = findViewById(R.id.et_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
 
         btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState != null){
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
     }
 
     override fun onClick(v: View?) {
-        if (v?.id == R.id.btn_hitung) {
-            val inputLength = evLength.text.toString().trim()
-            val inputWidth = evWidth.text.toString().trim()
-            val inputHeight = evHeight.toString().trim()
+        if (v?.id == R.id.btn_calculate) {
+            val inputLength = etLength.text.toString().trim()
+            val inputWidth = etWidth.text.toString().trim()
+            val inputHeight = etHeight.text.toString().trim()
 
-            val volume = inputLength.toDouble() * inputHeight.toDouble() * inputWidth.toDouble()
-            tvResult.text = volume.toString()
-
+//            Toast.makeText(applicationContext, "Tesssss", Toast.LENGTH_SHORT).show()
             var isEmptyFields = false
 
             if (inputHeight.isEmpty()){
                 isEmptyFields = true
-                evHeight.error = "Field ini tidak boleh kosong"
+                etHeight.error = "Field ini tidak boleh kosong"
             }
             if (inputLength.isEmpty()){
                 isEmptyFields = true
-                evLength.error = "Field ini tidak boleh kosong"
+                etLength.error = "Field ini tidak boleh kosong"
+            }
+            if (inputWidth.isEmpty()){
+                isEmptyFields = true
+                etWidth.error = "Field ini tidak boleh kosong"
+            }
+            if (!isEmptyFields){
+                val volume = inputLength.toDouble() * inputHeight.toDouble() * inputWidth.toDouble()
+                tvResult.text = volume.toString()
             }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
     }
 }
