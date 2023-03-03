e4b0ddf0cce4695750243005374d62bfb0bd0ccd
JosuaMnl <josuamnalu@gmail.com>
2023-03-03 16:57:38 +0700

    Refactor findViewById with View Binding

--- app/src/main/java/com/yosha10/barvolume/MainActivity.kt
+++ app/src/main/java/com/yosha10/barvolume/MainActivity.kt
@@ -7,13 +7,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.yosha10.barvolume.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var etWidth: EditText
-    private lateinit var etHeight: EditText
-    private lateinit var etLength: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
+//    private lateinit var etWidth: EditText
+//    private lateinit var etHeight: EditText
+//    private lateinit var etLength: EditText
+//    private lateinit var btnCalculate: Button
+//    private lateinit var tvResult: TextView
+    private lateinit var binding: ActivityMainBinding
 
     companion object {
         private const val STATE_RESULT = "state_result"
@@ -21,52 +23,53 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        etWidth = findViewById(R.id.et_width)
-        etHeight = findViewById(R.id.et_height)
-        etLength = findViewById(R.id.et_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
+//        etWidth = findViewById(R.id.et_width)
+//        etHeight = findViewById(R.id.et_height)
+//        etLength = findViewById(R.id.et_length)
+//        btnCalculate = findViewById(R.id.btn_calculate)
+//        tvResult = findViewById(R.id.tv_result)
 
-        btnCalculate.setOnClickListener(this)
+        binding.btnCalculate.setOnClickListener(this)
 
         if (savedInstanceState != null){
             val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text = result
+            binding.tvResult.text = result
         }
     }
 
     override fun onClick(v: View?) {
         if (v?.id == R.id.btn_calculate) {
-            val inputLength = etLength.text.toString().trim()
-            val inputWidth = etWidth.text.toString().trim()
-            val inputHeight = etHeight.text.toString().trim()
+            val inputLength = binding.etLength.text.toString().trim()
+            val inputWidth = binding.etWidth.text.toString().trim()
+            val inputHeight = binding.etHeight.text.toString().trim()
 
 //            Toast.makeText(applicationContext, "Tesssss", Toast.LENGTH_SHORT).show()
             var isEmptyFields = false
 
             if (inputHeight.isEmpty()){
                 isEmptyFields = true
-                etHeight.error = "Field ini tidak boleh kosong"
+                binding.etHeight.error = "Field ini tidak boleh kosong"
             }
             if (inputLength.isEmpty()){
                 isEmptyFields = true
-                etLength.error = "Field ini tidak boleh kosong"
+                binding.etLength.error = "Field ini tidak boleh kosong"
             }
             if (inputWidth.isEmpty()){
                 isEmptyFields = true
-                etWidth.error = "Field ini tidak boleh kosong"
+                binding.etWidth.error = "Field ini tidak boleh kosong"
             }
             if (!isEmptyFields){
                 val volume = inputLength.toDouble() * inputHeight.toDouble() * inputWidth.toDouble()
-                tvResult.text = volume.toString()
+                binding.tvResult.text = volume.toString()
             }
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, tvResult.text.toString())
+        outState.putString(STATE_RESULT, binding.tvResult.text.toString())
     }
 }
